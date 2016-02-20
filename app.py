54f60d0ee0e576f7dcfd2473b6b7c4418e62a1cb
juice500ml <juice500ml@gmail.com>
2016-02-20 12:22:07 +0000

    이제 회원가입이 됩니다!

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+from datetime import datetime
+
+from flask import Flask, jsonify, render_template, request
+from flask.ext.bcrypt import Bcrypt
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_migrate import Migrate
@@ -10,13 +13,16 @@ app.config.from_object(config)
 
 from db import (
     db,
+    User,
     URL,
     NickRecom,
 )
 db.init_app(app)
 migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
 
 admin = Admin(app)
+admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(URL, db.session))
 admin.add_view(ModelView(NickRecom, db.session))
 
@@ -26,16 +32,74 @@ def main():
     return 'main'
 
 
-@app.route("/newbie")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return '회원가입이 들어갈 자리입니다!'
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form['usr']
+        if not username: return 'Failed', 400
+        if len(username)>50: return 'Failed', 400
+        found = User.query.filter(
+                User.username == username,
+                ).first()
+        if found: return "Existing Username", 400
 
+        if not request.form['pwd']: return 'Failed', 400
+        if request.form['pwd'] != request.form['pwda']: return 'Failed', 400
+        password_hash = bcrypt.generate_password_hash(request.form['pwd'])
+
+        first_name_kr = request.form['fnk']
+        if not first_name_kr or len(first_name_kr)>50: return 'Failed', 400
+
+        last_name_kr = request.form['lnk']
+        if not last_name_kr or len(last_name_kr)>50: return 'Failed', 400
+
+        first_name_en = request.form['fne']
+        if not first_name_en or len(first_name_en)>50: return 'Failed', 400
+
+        middle_name_en = request.form['mne']
+        if len(middle_name_en)>50: return 'Failed', 400
+
+        last_name_en = request.form['lne']
+        if not last_name_en or len(last_name_en)>50: return 'Failed', 400
+
+        try:
+            student_number = int(request.form['sn'])
+        except ValueError:
+            return 'Failed', 400
+
+        last_login = datetime.now()
+
+        new_user = User()
+        new_user.username = username
+        new_user.password_hash = password_hash
+        new_user.first_name_kr = first_name_kr
+        new_user.last_name_kr = last_name_kr
+        new_user.first_name_en = first_name_en
+        new_user.middle_name_en = middle_name_en
+        new_user.last_name_en = last_name_en
+        new_user.student_number = student_number
+        new_user.last_login = last_login
+
+        db.session.add(new_user)
+        db.session.commit()
+        return "성공하였습니다!"
+
+@app.route("/check_username/<username>")
+def check_username(username):
+    found = User.query.filter(
+            User.username == username,
+    ).first()
+    if found:
+        return "Existing Username", 400
+    return "Good to go!"
 
 @app.route("/<link>")
 def goto(link):
     """jmg) 여러 링크를 입력해도 같은 아이디의 페이지로 이동하는 함수입니다."""
     found = URL.query.filter(
-        URL.link == link,
+            URL.link == link,
     ).first()
     if found:
         return userpage(found.ID)
@@ -85,10 +149,10 @@ def Search(id):
     ).all()
     result = {}
     for i in found:	
-	if i.nick in result:
-	    result[i.nick].append(i.fromA)
-	else:
-	    result[i.nick] = [i.fromA]
+        if i.nick in result:
+            result[i.nick].append(i.fromA)
+        else:
+            result[i.nick] = [i.fromA]
     return jsonify(result)
 
 
--- db.py
+++ db.py
@@ -5,6 +5,33 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 N = 50
 
+class User(db.Model):
+    username = db.Column(db.String(N), primary_key=True)
+    password_hash = db.Column(db.String(60), nullable=False)
+    first_name_kr = db.Column(db.String(N), nullable=False)
+    last_name_kr = db.Column(db.String(N), nullable=False)
+    first_name_en = db.Column(db.String(N), nullable=False)
+    middle_name_en = db.Column(db.String(N))
+    last_name_en = db.Column(db.String(N), nullable=False)
+    student_number = db.Column(db.Integer, nullable=False)
+    last_login = db.Column(db.DateTime, nullable=False)
+
+    """
+    def __init__(self, username, password_hash, first_name_kr, last_name_kr, first_name_en, middle_name_en, last_name_en, student_number, last_login):
+        self.username = username
+        self.password_hash = password_hash
+        self.first_name_kr = first_name_kr
+        self.last_name_kr = last_name_kr
+        self.first_name_en = first_name_en
+        self.middle_name_en = middle_name_en
+        self.last_name_en = last_name_en
+        self.student_number = student_number
+        self.last_login = last_login
+    """
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 class URL(db.Model):
     __tablename__ = "URL"
