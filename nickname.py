7144bf612b018745af55a28de3f7cd1fd645bc8e
JMG <jmg7173@sogang.ac.kr>
2016-02-29 23:51:27 +0900

    Issue #9 추천페이지 만들었어요 Issue #7 AddMyNick -> ManageMyNick register.html파일 수정했습니다.(제출->가입)

--- nickname.py
+++ nickname.py
@@ -2,6 +2,7 @@ from flask import (
     jsonify,
     render_template,
     redirect,
+    request,
 )
 
 from db import (
@@ -18,55 +19,40 @@ from user import (
 def add_routes(app):
     # TODO : Change URL
     app.route('/nickname/')(ManageMyNicknames)
-    app.route('/test/nick/recomm/<nick>/<target>/')(RecommendNickname)
-    app.route('/test/nick/recomm/')(RecommendedNicknamesForMe)
+    app.route('/test/nick/recomm/', methods=["GET", "POST"])(RecommendNickname)
     app.route('/test/mynick/', methods=["GET"])(ManageMyNicknames)
     app.route('/test/mynick/delete/<idx>/', methods=["POST"])(DelMyNick)
-    app.route('/test/mynick/add/<idx>/', methods=["POST"])(AddRecommNick)
+    app.route('/test/mynick/manage/<idx>/', methods=["POST"])(ManageRecommNick)
 
 
 # TODO : 자신이 자신 추천하면 안되요 안되.
-def RecommendNickname(nick, target):
+def RecommendNickname():
     """Issue #9, A라는 사용자가 B라는 사용자에게 nick이라는 별명을 추천하는 함수입니다.
     하지만 정민교(크하하하하)가 로그인 되었는지 알려주는 함수를 만들었기 때문에!
     A라는 사용자의 아이디를 매번 URL로 요청받을 필요가 없어졌습니다.
     """
     username = get_logged_in_username()
-    if not username:
-        return '로그인이 안되어 있습니다!!!!', 400
-    if not check_username(username, is_internal=True):
-        return '존재하지 않는 유저에게 추천하려고 하였습니다.', 400
-    new = NickRecom()
-    new.nick = nick
-    new.recommender = username
-    new.username = target
-    db.session.add(new)
-    db.session.commit()
-    return '추천되었습니다!'
-
-
-def RecommendedNicknamesForMe():
-    """Issue #9, 내가 추천받은 닉네임들을 보여줍니다."""
-    username = get_logged_in_username()
-    if not username:
-        return '로그인이 안되어 있다구욧!!!!', 400
-
-    # db 안의 toB 가 id와 일치하는 경우 모두(nick,from,toB) 출력할 것
-    found = NickRecom.query.filter(
-            NickRecom.username == username,
-    ).all()
-
-    result = {}
-    for i in found:
-        if i.nick in result:
-            result[i.nick].append(i.recommender)
-        else:
-            result[i.nick] = [i.recommender]
-    return jsonify(result)
+    if request.method == "GET":
+        if not username:
+            return '로그인이 안되어 있습니다!!!!', 400
+        return render_template("recommnick.html")
+    
+    else:
+        target = request.form['target']
+        if not check_username(target, is_internal=True):
+            return '존재하지 않는 유저에게 추천하려고 하였습니다.', 400
+        new = NickRecom()
+        new.nick = request.form['nick']
+        new.recommender = username
+        new.username = target
+        db.session.add(new)
+        db.session.commit()
+        return '추천되었습니다!'
 
 
 def ManageMyNicknames():
     """
+    Issue #7 내가 추천받은 닉네임들과 사용중인 닉네임들을 관리합니다.
     1. 내 닉네임들을 보여줌
     2. 내 닉네임들을 빼거나 추가할 수 있음.
       - 닉네임 등록 시 url db에 추가할지 물어보고, True일경우 추가해줌.
@@ -98,6 +84,7 @@ def ManageMyNicknames():
 
 
 def DelMyNick(idx):
+    # Issue #7 내가 사용중인 닉네임을 삭제합니다.
     found = Nickname.query.filter(
         Nickname.idx == int(idx),
     ).first()
@@ -112,7 +99,8 @@ def DelMyNick(idx):
 
 
 # TODO:나중에 추천받은 닉네임들이 중복되는 경우 하나만 출력하게 한 후 idx말고 nick으로 받아올 것.
-def AddRecommNick(idx):
+def ManageRecommNick(idx):
+    # Issue #7 내가 추천받은 닉네임을 관리합니다. submit==use: 사용 else: 거절
     found_recomm = NickRecom.query.filter(
         NickRecom.idx == idx,
     ).first()
@@ -122,20 +110,25 @@ def AddRecommNick(idx):
         return 'ㅗㅗ', 400
 
     nick = found_recomm.nick
+    if request.form.get('use', None):
+        found = NickRecom.query.filter(
+            NickRecom.username == username,
+            NickRecom.nick == nick,
+        ).first()
+        add_nick = Nickname()
+        add_nick.username = username
+        add_nick.nick = nick
+        add_nick.recommender = found.recommender
+        db.session.add(add_nick)
 
+    RemoveRecommNick(nick)
+    db.session.commit()
+    return redirect("/test/mynick/")
+
+def RemoveRecommNick(nick):
+    # Issue #7 추천받은 닉네임을 DB에서 삭제합니다.
     found = NickRecom.query.filter(
-        NickRecom.username == username,
-        NickRecom.nick == nick,
-    ).first()
-
-    add_nick = Nickname()
-    add_nick.username = username
-    add_nick.nick = nick
-    add_nick.recommender = found.recommender
-    db.session.add(add_nick)
-
-    found = NickRecom.query.filter(
-        NickRecom.username == username,
+        NickRecom.username == get_logged_in_username(),
         NickRecom.nick == nick,
     ).all()
 
@@ -146,4 +139,4 @@ def AddRecommNick(idx):
         ).first()
         db.session.delete(del_from_Recomm)
     db.session.commit()
-    return redirect("/test/mynick/")
+    return
