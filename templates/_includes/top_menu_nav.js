5ceb5c54b6cded7a49e81bfa4b7e7d7d82b04ca0
yunitto <helen7908@gmail.com>
2016-03-06 11:29:09 +0000

    Issue #83 solved thanks to minhoryang

--- templates/_includes/top_menu_nav.js
+++ templates/_includes/top_menu_nav.js
@@ -1,3 +1,6 @@
 <script>
-    $('.new-nickname-alert').popover('show')
+    $('.new-nickname-alert').popover({
+        'template': '<div class="popover" role="tooltip"><div class="popover-arrow"></div><button type="button" class="close" aria-hidden="true" onclick=$(".new-nickname-alert").popover("hide")>&times;</button><div class="popover-content"></div></div>'
+    });
+    $('.new-nickname-alert').popover('show');
 </script>
