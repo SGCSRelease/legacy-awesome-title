849c19d787a550ce9edfd53aa0ab0521ba1c5726
JimJeon <jimjeon.me@gmail.com>
2018-04-30 07:07:13 +0000

    [NEW] add to_dict to make db json

--- AwesomeTitleServer/db.py
+++ AwesomeTitleServer/db.py
@@ -19,6 +19,17 @@ class User(db.Model):
     last_login = db.Column(db.DateTime, nullable=False)
 
 
+    def to_dict(self):
+        # TODO: _link element for using api contents
+        return {
+            'username': self.username,
+            'realname': self.realname,
+            'student_number': self.student_number,
+            'last_login': self.last_login.isoformat() + 'Z'
+        }
+
+
+
 class URL(db.Model):
     __tablename__ = "URL"
     link = db.Column(db.String(N), primary_key=True)
@@ -33,12 +44,31 @@ class Nickname(db.Model):
     recommender = db.Column(db.String(N))
 
 
+    def to_dict(self):
+        # TODO: _link element for using api contents
+        return {
+            'id': self.idx,
+            'username': self.username,
+            'nickname': self.nick,
+            'recommender': self.recommender,
+        }
+
+
 class NickRecom(db.Model):
     __tablename__ = "nickrecom"
     idx = db.Column(db.Integer, primary_key=True)
     nick = db.Column(db.String(N))
     recommender = db.Column(db.String(N))
     username = db.Column(db.String(N))
+
+
+    def to_dict(self):
+        return {
+            'id': self.idx,
+            'username': self.username,
+            'nickname': self.nick,
+            'recommender': self.recommender,
+        }
 
 
 class Photo(db.Model):
