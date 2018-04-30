678950b5a426f04dc93c755bcdfb10aeefaeb6a6
JimJeon <jimjeon.me@gmail.com>
2018-04-30 07:05:20 +0000

    [NEW] add nickname rest api

---
+++ AwesomeTitleServer/api/nickname.py
@@ -0,0 +1,62 @@
+# Standard Library
+
+# Third Party
+from flask import (
+    jsonify,
+)
+
+# Local Application
+from AwesomeTitleServer.db import (
+    User,
+    Nickname,
+    NickRecom,
+)
+from AwesomeTitleServer.api import bp as api
+
+
+@api.route('/nickname/<username>', methods=['GET'])
+def get_nickname(username):
+    found = Nickname.query.filter(
+        Nickname.username == username
+    ).all()
+
+    return [jsonify(nick.to_dict()) for nick in found]
+
+
+@api.route('/nickname/<username>', methods=['POST'])
+def create_nickname(username):
+    pass
+
+
+@api.route('/nickname/<username>', methods=['PUT'])
+def update_nickname(username):
+    pass
+
+
+@api.route('/nickname/<username>', methods=['DELETE'])
+def delete_nickname(username):
+    pass
+
+
+@api.route('/nickname/recommend/<username>', methods=['GET'])
+def get_nickrecom(username):
+    found = NickRecom.query.filter(
+        NickRecom.username == username
+    ).all()
+
+    return [jsonify(nick.to_dict()) for nick in found]
+
+
+@api.route('/nickname/recommend/<username>', methods=['POST'])
+def create_nickrecom(username):
+    pass
+
+
+@api.route('/nickname/recommend/<username>', methods=['PUT'])
+def update_nickrecom(username):
+    pass
+
+
+@api.route('/nickname/recommend/<username>', methods=['DELETE'])
+def delete_nickrecom(username):
+    pass
