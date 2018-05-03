31b47e5e0b96475eb49c168fc78cac6cd885b17e
JimJeon <jimjeon.me@gmail.com>
2018-05-03 10:56:59 +0000

    [NEW] add decorator login_required
    @login_required checks if it logged in.

---
+++ AwesomeTitleServer/auth/decorators.py
@@ -0,0 +1,20 @@
+# Standard Library
+
+# Third Party
+from flask import (
+    render_template,
+)
+
+# Local Application
+from AwesomeTitleServer.user import get_logged_in_username
+
+
+def login_required(func):
+    def wrapper_function():
+        if not get_logged_in_username():
+            return render_template(
+                '_error.html',
+                _error__msg='Not logged in',
+            )
+        return func
+    return wrapper_function
