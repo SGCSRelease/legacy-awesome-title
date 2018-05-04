6182b6991dfb42ad278f3b981b7990c3de9efd7f
JimJeon <jimjeon.me@gmail.com>
2018-05-04 08:51:15 +0000

    [FIX] fix decorator parameters

--- AwesomeTitleServer/auth/decorators.py
+++ AwesomeTitleServer/auth/decorators.py
@@ -10,11 +10,11 @@ from AwesomeTitleServer.user import get_logged_in_username
 
 
 def login_required(func):
-    def wrapper_function():
+    def wrapper_function(*args, **kwargs):
         if not get_logged_in_username():
             return render_template(
                 '_error.html',
                 _error__msg='Not logged in',
             )
-        return func
+        return func(*args, **kwargs)
     return wrapper_function
